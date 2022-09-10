eb989ae64df0cc3e6f81e65be5cdfeab6216bb14
sorkihg <sorkihg@gmail.com>
2022-09-10 23:20:59 +0900

    model 일부 수정

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -6,10 +6,10 @@ from apps.users.models import User
 class WantedData(models.Model):
     delivery_state = models.ForeignKey("orders.DeliveryState", on_delete=models.CASCADE)
     buyer          = models.ForeignKey(User, on_delete=models.CASCADE)
-    country_code   = models.ForeignKey("orders.CountryCode", on_delete=models.CASCADE)
+    pay_state      = models.CharField(max_length=50)
     quantity       = models.IntegerField()
     price          = models.DecimalField(max_digits = 8, decimal_places = 2)
-    created_at     = models.DateTimeField(auto_now_add=True)
+    date           = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = "wanted_data"
