8f59383ec7cf3e3ba552f06bc4665d9317dbdb37
sorkihg <sorkihg@gmail.com>
2022-09-13 01:59:10 +0900

    주문내역에 대한 상세페이지 조회 및 배송상태 업데이트 API
     1. RetrieveUpdateAPIView를 활용해 구현
     2. 배송상태의 UPDATE는 배송준비, 배송중, 배송완료, 주문취소 4개로
        수정가능구현, 이외의 데이터가 들어올시 에러처리 구현

--- apps/orders/serializers.py
+++ apps/orders/serializers.py
@@ -1,12 +1,18 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
-from apps.orders.models import WantedData
+from apps.orders.models import DeliveryState, WantedData
 from apps.users.serializers import UserSerializer
 
 
+class DeliveryStateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DeliveryState
+        fields = "__all__"
+
 class OrderSerializer(serializers.ModelSerializer):
     buyer          = UserSerializer(read_only = True)
-    delivery_state = serializers.StringRelatedField()
+    delivery_state = DeliveryStateSerializer()
 
     class Meta:
         model = WantedData
@@ -19,3 +25,24 @@ class OrderSerializer(serializers.ModelSerializer):
             "price", 
             "date" 
             ]
+        read_only_fields = ("price", "quantity", "pay_state")
+
+    def validate(self, validated_data):
+        '''
+        배송상태 UPDATA를 위한 검사
+        배송준비, 배송중, 배송완료, 주문취소 4개 이외의 데이터가 들어올시 에러처리
+        '''
+        if self.instance:
+            data = validated_data['delivery_state']
+            if not DeliveryState.objects.filter(name = data['name']).exists():
+                raise ValidationError("잘못수정하였습니다. 배송준비, 배송중, 배송완료, 주문취소 중에 하나로 입력해주세요")
+        return validated_data
+
+    def update(self, instance, validated_data):
+        data = validated_data['delivery_state']
+        data = DeliveryState.objects.get(name = data['name'])
+        instance.delivery_state = data
+        
+        instance.save()
+        return instance
+    
--- apps/orders/urls.py
+++ apps/orders/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from apps.orders.views import OrderListView
+from apps.orders.views import OrderListView, OrderDetailUpdateView
 urlpatterns = [
-    path("", OrderListView.as_view())
+    path("", OrderListView.as_view()),
+    path("<int:pk>/", OrderDetailUpdateView.as_view()),
 ]
