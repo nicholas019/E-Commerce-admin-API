5a1f553dfb7d439caa9dfe00d500ef6db63cfb72
sorkihg <sorkihg@gmail.com>
2022-09-14 19:38:45 +0900

    fix : 타입별 사용횟수 기능 추가

--- apps/coupons/views.py
+++ apps/coupons/views.py
@@ -1,10 +1,11 @@
-from django.db.models import Q
+from django.db.models import Q, Count
 
 from rest_framework import generics
 from rest_framework.filters import SearchFilter
 
 from apps.coupons.models import Coupon, CouponIssueList
 from apps.coupons.serializers import CouponCreateSerializer, CouponIssueSerializer
+from apps.coupons.pagination import CouponPageNumberPagination
 
 
 class CouponListRegisterView(generics.ListCreateAPIView):
@@ -16,28 +17,31 @@ class CouponListRegisterView(generics.ListCreateAPIView):
         필터링3(필터링1+2 동시사용) : 사용완료이면서 배송비무료쿠폰, 사용완료이면서 1000원할인쿠폰 등 조합 가능, 예) /?search=1&q=2
         발급 쿠폰 등록 : /api/coupon/new/ 에서 발급된 쿠폰번호를 입력하면 선정된 유저에 등록
     에러처리 : 발급쿠폰 등록시 사용된 쿠폰이면 "쿠폰번호가 이미 사용되었습니다." 에러메시지 반환
+    페이지네이션 기능 추가 페이지당 게시글수는 20개
+    타입별 사용횟수확인은 사용완료(search=1)와 쿠폰타입으로 필터를 하면 상단의 "count" 값으로 출력
     '''
     serializer_class = CouponIssueSerializer
-    
+    pagination_class = CouponPageNumberPagination
     filter_backends = [SearchFilter]
     search_fields = ('is_use', )
 
     # 필터링 조합을 위해 get_queryset 오버라이딩하여 커스텀 진행
-    def get_queryset(self, *args, **kwargs):
-        queryset_list = CouponIssueList.objects.all().select_related("user", "coupon", "coupon__coupon_type")
+    def get_queryset(self):
+        queryset = CouponIssueList.objects.all().select_related("user", "coupon", "coupon__coupon_type")
         query = self.request.GET.get("q")
         if query:
-            queryset_list = queryset_list.filter(
-                Q(coupon__coupon_type__id__icontains=query) 
-            ).distinct()
-        return queryset_list
+            queryset = queryset.filter(
+                Q(coupon__coupon_type__id__icontains=query)
+            )
+        return queryset
 
 
 class CouponCreateView(generics.CreateAPIView):
     '''
     신규 쿠폰 생성 API
-    기능설명 : 신규 쿠폰번호와 쿠폰타입을 지정하면 신규쿠폰 생성
+    기능설명 : 신규 쿠폰번호와 쿠폰타입을 지정하면 신규쿠폰 생성, 쿠폰번호는 정수만 사용 가능
     에러처리 : 쿠폰번호 중복시 "쿠폰번호가 중복입니다. 쿠폰번호를 확인해주세요." 에러메세지 반환
     '''
     queryset = Coupon.objects.all()
     serializer_class = CouponCreateSerializer
+    
